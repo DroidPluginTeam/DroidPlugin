366d8c51487613de4e8452bc5a5d7061aadfacd5
bb.S <bangbang.S@gmail.com>
2015-09-23 13:29:25 +0800

    add copyright

--- project/TestPlugin/src/com/example/TestPlugin/AllLauncherActivity.java
+++ project/TestPlugin/src/com/example/TestPlugin/AllLauncherActivity.java
@@ -1,3 +1,25 @@
+/*
+**        DroidPlugin Project
+**
+** Copyright(c) 2015 bb.S <bangbang.song@gmail.com>
+**
+** This file is part of DroidPlugin.
+**
+** DroidPlugin is free software: you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation, either
+** version 3 of the License, or (at your option) any later version.
+**
+** DroidPlugin is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with DroidPlugin.  If not, see <http://www.gnu.org/licenses/lgpl.txt>
+**
+**/
+
 package com.example.TestPlugin;
 
 import android.app.Activity;
