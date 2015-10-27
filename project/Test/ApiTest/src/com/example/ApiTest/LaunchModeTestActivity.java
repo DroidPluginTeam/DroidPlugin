3382bc6d1664c2dfe5601ba640fb83fb1219d0f1
Andy Zhang <zhangyong232@gmail.com>
2015-10-27 16:21:20 +0800

    setOnClickListener

--- project/Test/ApiTest/src/com/example/ApiTest/LaunchModeTestActivity.java
+++ project/Test/ApiTest/src/com/example/ApiTest/LaunchModeTestActivity.java
@@ -29,6 +29,21 @@ public abstract class LaunchModeTestActivity extends Activity implements View.On
         findViewById(R.id.button2).setOnClickListener(this);
         findViewById(R.id.button3).setOnClickListener(this);
         findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        findViewById(R.id.button6).setOnClickListener(this);
+        findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button9).setOnClickListener(this);
+        findViewById(R.id.button10).setOnClickListener(this);
+        findViewById(R.id.button11).setOnClickListener(this);
+        findViewById(R.id.button12).setOnClickListener(this);
+        findViewById(R.id.button13).setOnClickListener(this);
+        findViewById(R.id.button14).setOnClickListener(this);
+        findViewById(R.id.button15).setOnClickListener(this);
+        findViewById(R.id.button16).setOnClickListener(this);
+        findViewById(R.id.button17).setOnClickListener(this);
+        findViewById(R.id.button18).setOnClickListener(this);
+        findViewById(R.id.button19).setOnClickListener(this);
     }
 
     @Override
