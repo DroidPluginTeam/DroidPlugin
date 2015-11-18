16088fbc9266484889e126f2187c08a8c433968d
Andy Zhang <zhangyong232@gmail.com>
2015-11-18 15:25:38 +0800

    Bug fix:For QiKU
    部分带Support V4的插件无法在奇酷手机上运行的问题。
    
    这里适配奇酷手机青春版。
    
    因为奇酷手机加载了自己修改过的的Support V4库，在插件中也用了这个库的时候，ClassLoader会优先加载奇酷手机自带的Support V4库。原因在于，奇酷手机没有预加载插件中的Support V4库。详情可以研究super.loadClass(className, resolve)标准实现.
    
    但是这可能会导致类不兼容，出现java.lang.IncompatibleClassChangeError。因为插件编译时使用的插件的Support V4，而奇酷手机则使用的是它修改过的Support V4。
    
    SO,在Class Loader加载某个Class的时候，我们优先从自己的ClassLoader中加载Class，如果找不到，再从Parent Class Loader中去加载。
    
    这样修改后，Class的加载顺序就跟系统的不一样了。

--- project/Libraries/DroidPlugin/src/com/morgoo/droidplugin/core/PluginClassLoader.java
+++ project/Libraries/DroidPlugin/src/com/morgoo/droidplugin/core/PluginClassLoader.java
@@ -22,6 +22,11 @@
 
 package com.morgoo.droidplugin.core;
 
+import android.os.Build;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import dalvik.system.DexClassLoader;
 
 /**
@@ -31,5 +36,38 @@ public class PluginClassLoader extends DexClassLoader {
 
     public PluginClassLoader(String apkfile, String optimizedDirectory, String libraryPath, ClassLoader systemClassLoader) {
         super(apkfile, optimizedDirectory, libraryPath, systemClassLoader);
+    }
+
+    private static final List<String> sPreLoader = new ArrayList<>();
+
+    static {
+        sPreLoader.add("QIKU");
+    }
+
+    @Override
+    protected Class<?> loadClass(String className, boolean resolve) throws ClassNotFoundException {
+
+        if (Build.MANUFACTURER != null && sPreLoader.contains(Build.MANUFACTURER.toUpperCase())) {
+            try {
+                /**
+                 * FUCK QIKU！
+                 * 这里适配奇酷手机青春版。
+                 * 因为奇酷手机自己加载了自己修改过的的Support V4库，在插件中也用了这个库的时候，ClassLoader会优先加载奇酷手机自带的Support V4库。
+                 * 原因在于，奇酷手机没有预加载插件中打的Support V4库。详情可以研究super.loadClass(className, resolve)标准实现
+                 * 但是这可能会导致类不兼容，出现java.lang.IncompatibleClassChangeError。因为插件编译时使用的插件的Support V4，而奇酷手机则使
+                 * 用的是它修改过的Support V4。
+                 *
+                 * SO,在Class Loader加载某个Class的时候，我们优先从自己的ClassLoader中加载Class，如果找不到，再从Parent Class Loader中去加载。
+                 * 这样修改后，Class的加载顺序就跟系统的不一样了。
+                 *
+                 */
+                Class<?> clazz = findClass(className);
+                if (clazz != null) {
+                    return clazz;
+                }
+            } catch (ClassNotFoundException e) {
+            }
+        }
+        return super.loadClass(className, resolve);
     }
 }
